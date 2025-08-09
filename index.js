5e594b0091e4b0f6acdca8c22f443053ee18609a
InfiniteChaos248 <svarjun1997@gmail.com>
2025-08-09 10:22:13 +0530

    homepage

--- index.js
+++ index.js
@@ -1,65 +1,10 @@
 window.onload = function () {
-	const hamburger = document.getElementById('hamburger');
-	const headings = document.getElementById('heading-section');
-	const main = document.getElementById('main');
-	const navbar = document.getElementById('navbar');
-
-	function toggle_hamburger_menu() {
-		headings.classList.toggle('show');
-		setTimeout(() => {
-			const navHeight = navbar.offsetHeight;
-			main.style.marginTop = `${navHeight}px`;
-		}, 10);
-	}
-
-	hamburger.addEventListener('click', toggle_hamburger_menu);
-
-	const logo = document.getElementById('logo');
-	const nav_paxc25 = document.getElementById('nav-paxc25');
-	const nav_ai25 = document.getElementById('nav-ai25');
-	const nav_quiz = document.getElementById('nav-quiz');
-	const nav_about = document.getElementById('nav-about');
-
-	const home = document.getElementById('home');
-	const paxc25 = document.getElementById('paxc25');
-	const ai25 = document.getElementById('ai25');
-	const quiz = document.getElementById('quiz');
-	const about = document.getElementById('about');
-
-	logo.addEventListener('click', () => {
-		paxc25.style.display = 'none';
-		home.style.display = 'block';
-		ai25.style.display = 'none';
-		quiz.style.display = 'none';
-		about.style.display = 'none';
+	let counter = 0;
+	const main_logo = document.getElementById('main-logo');
+	main_logo.addEventListener('click', () => {
+			counter = counter + 1;
+			if(counter == 1) {
+				alert('hello there!');
+			}
 	});
-
-	nav_paxc25.addEventListener('click', () => {
-		paxc25.style.display = 'block';
-		home.style.display = 'none';
-		ai25.style.display = 'none';
-		quiz.style.display = 'none';
-		about.style.display = 'none';
-	});
-	nav_ai25.addEventListener('click', () => {
-		paxc25.style.display = 'none';
-		home.style.display = 'none';
-		ai25.style.display = 'block';
-		quiz.style.display = 'none';
-		about.style.display = 'none';
-	});
-	nav_quiz.addEventListener('click', () => {
-		paxc25.style.display = 'none';
-		home.style.display = 'none';
-		ai25.style.display = 'none';
-		quiz.style.display = 'flex';
-		about.style.display = 'none';
-	});
-	nav_about.addEventListener('click', () => {
-		paxc25.style.display = 'none';
-		home.style.display = 'none';
-		ai25.style.display = 'none';
-		quiz.style.display = 'none';
-		about.style.display = 'block';
-	});
-};
+}
